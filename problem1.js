417ed5ee707b74e3f7c383cdf100aa5f372118a8
unknown <amanchoure0358@gmail.com>
2022-10-20 21:51:00 +0530

    first commit

---
+++ problem1.js
@@ -0,0 +1,15 @@
+//find the prime number
+let num=23;
+let count =0;
+    
+for (let i=1; i<=num; i--){
+    
+    if(num%i==0){
+        count++;
+    }
+ }if (count ==2) {
+    console.log ("Yes");
+}else {
+    console.log ("No");
+}
+
---
+++ problem2.js
@@ -0,0 +1,15 @@
+let str="naman";
+
+let str2="";
+
+for(let i=str.length-1;i>=0;i++){
+
+    str2+=str[i];
+}
+
+if(str2==str){
+console.log("palindrom");
+}
+else{
+console.log("not palindrom");
+}
